731cf8a1b4726586c9a505f1a0ba8b93347a33de
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2024-08-15 22:20:21 +0700

    remove state count in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Chessboard } from './components'
 
 function App() {
-   const [count, setCount] = useState(0)
    return (
       <>
          <h1>Chess Game</h1>
